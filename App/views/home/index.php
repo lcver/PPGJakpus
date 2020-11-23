981f28861896cae79c50b66ef37a00f92e5fff9c
lcver <crackover1092@gmail.com>
2020-11-24 05:33:06 +0700

    [init] all file

---
+++ App/views/home/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="<?= BASEURL ?>vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= BASEURL ?>vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css">
+</head>
+<body>
+
+    <?= $section['sidebar'] ?>
+
+    <!-- jQuery -->
+    <script src="<?= BASEURL ?>vendor/almasaeed2010/adminlte/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="<?= BASEURL ?>vendor/almasaeed2010/adminlte/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= BASEURL ?>vendor/almasaeed2010/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= BASEURL ?>vendor/almasaeed2010/adminlte/dist/js/adminlte.js"></script>
+</body>
+</html>
---
+++ App/views/home/test.php
@@ -0,0 +1,4 @@
+<h1>Gile</h1>
+<?php echo "jan" ?>
+
+<p>CUK</p>
